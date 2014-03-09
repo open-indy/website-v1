7d96302e01d4308d3c8ec5ca433b4145d3576f65
Brian <brian@socialhealthinsights.com>
2014-03-09 17:58:39 -0400

    More new content
    Added Postgress SQL database and challenge class. Also new index page
    content and started a page to enter a challenge (which needs more work)

--- Gemfile
+++ Gemfile
@@ -3,3 +3,10 @@ source "https://rubygems.org"
 ruby '2.1.0'
 
 gem 'sinatra'
+gem 'rake'
+gem 'json'
+gem 'data_mapper'
+gem 'dm-timestamps'
+gem 'dm-core'
+gem 'dm-migrations'
+gem 'dm-postgres-adapter'
--- app.rb
+++ app.rb
@@ -7,6 +7,22 @@ require 'open-uri'
 
 Bundler.require
 
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/IndCivicHack")
+
+class Challenge
+  include DataMapper::Resource
+  property :id, Serial
+  property :first_name, Text
+  property :last_name, Text
+  property :email, Text
+  property :organization, Text
+  property :challenge_desc, Text
+  property :apis, Text
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
 
 ##############################################
 
@@ -14,7 +30,6 @@ get "/" do
   erb :index
 end
 
-get "/map" do
-  erb :map
+get "/newchallenge" do
+	erb :newchallenge
 end
-
