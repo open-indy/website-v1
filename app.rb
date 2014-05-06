f2214fff6e32240d48a44ec410f06c20971cf739
Brian <brian@socialhealthinsights.com>
2014-05-06 10:39:56 -0400

    Schedule changes - removed Postgress DB code

--- app.rb
+++ app.rb
@@ -8,21 +8,21 @@ require 'open-uri'
 
 Bundler.require
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/IndCivicHack")
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/IndCivicHack")
 
-class Challenge
-  include DataMapper::Resource
-  property :id, Serial
-  property :name, Text
-  property :email, Text
-  property :organization, Text
-  property :challenge_desc, Text
-  property :apis, Text
-  property :created_at, DateTime
-  property :updated_at, DateTime
-end
+# class Challenge
+  # include DataMapper::Resource
+  # property :id, Serial
+  # property :name, Text
+  # property :email, Text
+  # property :organization, Text
+  # property :challenge_desc, Text
+  # property :apis, Text
+  # property :created_at, DateTime
+  # property :updated_at, DateTime
+# end
 
-DataMapper.finalize.auto_upgrade!
+# DataMapper.finalize.auto_upgrade!
 
 # use Rack::Auth::Basic, "Protected Area" do |username, password|
 # username == '' && password == ''
@@ -31,8 +31,8 @@ DataMapper.finalize.auto_upgrade!
 ##############################################
 
 get "/" do
-	@data = Challenge.all
-	@challengecount = DataMapper.repository.adapter.select("SELECT COUNT(id) FROM challenges").first.to_s
+	# @data = Challenge.all
+	# @challengecount = DataMapper.repository.adapter.select("SELECT COUNT(id) FROM challenges").first.to_s
 	erb :index
 end
 
@@ -45,18 +45,18 @@ get "/localchallenge" do
 	erb :localchallenge
 end
 
-post '/newchallenge' do
-	n = Challenge.new
-	n.name = params[:name]
-	n.email = params[:email]
-	n.organization = params[:organization]
-	n.challenge_desc = params[:challenge_desc]
-	n.apis = params[:apis]
-	n.created_at = Time.now
-	n.updated_at = Time.now
-	n.save
-	redirect '/'
-end
+# post '/newchallenge' do
+	# n = Challenge.new
+	# n.name = params[:name]
+	# n.email = params[:email]
+	# n.organization = params[:organization]
+	# n.challenge_desc = params[:challenge_desc]
+	# n.apis = params[:apis]
+	# n.created_at = Time.now
+	# n.updated_at = Time.now
+	# n.save
+	# redirect '/'
+# end
 
 get '/terms' do 
 	erb :terms
